dfd849270fbb477abffb830224bce34a2827f0ec
Davide Iacovone <iacovone.david@gmail.com>
2022-06-12 12:54:54 +0200

    Connecting to SQL server

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Countify.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("MainDatabase"));
+        }
+    }
+}
