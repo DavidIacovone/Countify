23a29c100dcaf379cc4a77a63c0b1081b8c75680
Davide Iacovone <iacovone.david@gmail.com>
2022-06-19 02:00:22 +0200

    TaxController updated to use try catch structure

--- Controllers/TaxController.cs
+++ Controllers/TaxController.cs
@@ -25,15 +25,22 @@ public class TaxController : ControllerBase
     [Route("UpdateBalance")]
     public async Task<ActionResult<decimal>> UpdateBalance(decimal change, string email)
     {
-        //finding the user 
-        var user = await UsersService.GetByEmail(email);
-        if (user == null) return BadRequest("This email is not registered");
+        try
+        {
+            //finding the user 
+            var user = await UsersService.GetByEmail(email);
+            if (user == null) return BadRequest("This email is not registered");
 
-        //updating the balance
-        user.Balance = user.Balance + change;
-        var updatedUser = await UsersService.Update(user);
+            //updating the balance
+            user.Balance = user.Balance + change;
+            var updatedUser = await UsersService.Update(user);
 
-        return Ok(updatedUser.Balance);
+            return Ok(updatedUser.Balance);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
     }
 
     [HttpPost]
@@ -41,8 +48,15 @@ public class TaxController : ControllerBase
     [Route("AddPenalty")]
     public async Task<ActionResult<Penalty>> AddPenalty(Penalty penalty)
     {
-        var createdPenalty = await PenaltyService.Add(penalty);
-        return Ok(createdPenalty);
+        try
+        {
+            var createdPenalty = await PenaltyService.Add(penalty);
+            return Ok(createdPenalty);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
     }
 
     [HttpPost]
@@ -50,19 +64,26 @@ public class TaxController : ControllerBase
     [Route("AddFees")]
     public async Task<ActionResult<string>> AddFees()
     {
-        var users = await UsersService.GetAll();
-        foreach (var user in users)
-            if (user.Role == Role.SubMember)
-            {
-                user.Balance = user.Balance - 20;
-                await UsersService.Update(user);
-            }
-            else
-            {
-                user.Balance = user.Balance - 50;
-                await UsersService.Update(user);
-            }
+        try
+        {
+            var users = await UsersService.GetAll();
+            foreach (var user in users)
+                if (user.Role == Role.SubMember)
+                {
+                    user.Balance = user.Balance - 20;
+                    await UsersService.Update(user);
+                }
+                else
+                {
+                    user.Balance = user.Balance - 50;
+                    await UsersService.Update(user);
+                }
 
-        return Ok("Fees applied");
+            return Ok("Fees applied");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
     }
 }
